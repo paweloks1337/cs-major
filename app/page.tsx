bbbae1bfd6e11e1504eb222a41fe3ba6e08f4a5b
paweloks1337 <paweloxbieniek1@gmail.com>
2025-11-20 22:13:11 +0100

    Fix useSession prerender error

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,34 @@
 "use client";
+export const dynamic = "force-dynamic";
 
 import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession();
+  const { data: session } = useSession() || {};
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-      <h1>Hello CS Major!</h1>
+    <div style={{ padding: "40px", textAlign: "center", fontSize: "20px" }}>
+      <h1>CS Major</h1>
 
-      {session ? (
-        <>
-          <p>Logged in as {session.user?.name}</p>
-          <button onClick={() => signOut()} style={{ marginTop: 20, padding: "10px 20px", cursor: "pointer" }}>
-            Log out
-          </button>
-        </>
-      ) : (
-        <button onClick={() => signIn("discord")} style={{ marginTop: 20, padding: "10px 20px", cursor: "pointer" }}>
-          Log in with Discord
+      {!session && (
+        <button
+          onClick={() => signIn("discord")}
+          style={{ padding: "10px 20px", marginTop: "20px" }}
+        >
+          Login with Discord
         </button>
+      )}
+
+      {session && (
+        <div>
+          <p>Logged in as {session.user?.name}</p>
+          <button
+            onClick={() => signOut()}
+            style={{ padding: "10px 20px", marginTop: "20px" }}
+          >
+            Logout
+          </button>
+        </div>
       )}
     </div>
   );
