d123a368162f2a1887942c7e5c42f03ce6e235f1
paweloks1337 <paweloxbieniek1@gmail.com>
2025-11-20 21:29:49 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,26 @@
 "use client";
-import { signIn } from "next-auth/react";
+
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
+
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh" }}>
       <h1>Hello CS Major!</h1>
-      <button onClick={() => signIn("discord")} style={{ marginTop: "20px", padding: "10px 20px", cursor: "pointer" }}>
-        Log in with Discord
-      </button>
+
+      {session ? (
+        <>
+          <p>Logged in as {session.user?.name}</p>
+          <button onClick={() => signOut()} style={{ marginTop: 20, padding: "10px 20px", cursor: "pointer" }}>
+            Log out
+          </button>
+        </>
+      ) : (
+        <button onClick={() => signIn("discord")} style={{ marginTop: 20, padding: "10px 20px", cursor: "pointer" }}>
+          Log in with Discord
+        </button>
+      )}
     </div>
   );
 }
